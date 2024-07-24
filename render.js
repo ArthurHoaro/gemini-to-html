3726bba762a630929ab30ca8e481a81e9fe87174
ArthurHoaro <arthur@hoa.ro>
2024-07-24 12:34:48 -0400

    optional classname

--- render.js
+++ render.js
@@ -12,16 +12,17 @@ function id(content) {
 function render (tokens) {
   return tokens.map((line) => {
     const { type } = line
+    const className = line.className ? ` class="${line.className}"` : ''
 
     switch (type) {
-      case 'quote': return htmlEscape`<blockquote class="${line.className}">${line.content}</blockquote>`
-      case 'header': return htmlEscape`<h${line.level} id="${id(line.content)}" class="${line.className}">${line.content}</h${line.level}>`
-      case 'link': return htmlEscape`<p><a href="${line.href}" class="${line.className}">${line.content}</a></p>`
+      case 'quote': return htmlEscape`<blockquote${className}>${line.content}</blockquote>`
+      case 'header': return htmlEscape`<h${line.level} id="${id(line.content)}"${className}>${line.content}</h${line.level}>`
+      case 'link': return htmlEscape`<p><a href="${line.href}"${className}>${line.content}</a></p>`
       case 'pre': return line.alt
-        ? htmlEscape`<pre class="${line.className}"><code class="language-${line.alt}">\n${line.items.join('\n')}\n</code></pre>`
-        : htmlEscape`<pre class="${line.className}">\n${line.items.join('\n')}\n</pre>`
-      case 'list': return `<ul class="${line.className}">\n${line.items.map((item) => htmlEscape`\t<li>${item}</li>`).join('\n')}\n</ul>`
-      default: return line.content ? htmlEscape`<p class="${line.className}">${line.content}</p>` : '<br/>'
+        ? htmlEscape`<pre${className}><code class="language-${line.alt}">\n${line.items.join('\n')}\n</code></pre>`
+        : htmlEscape`<pre${className}>\n${line.items.join('\n')}\n</pre>`
+      case 'list': return htmlEscape`<ul${className}>\n${line.items.map((item) => htmlEscape`\t<li>${item}</li>`).join('\n')}\n</ul>`
+      default: return line.content ? htmlEscape`<p${className}>${line.content}</p>` : '<br/>'
     }
   }).join('\n')
 }
