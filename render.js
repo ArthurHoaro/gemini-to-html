0ae400d24dd79353266a18d8d9a25dadb037ec0d
ArthurHoaro <arthur@hoa.ro>
2024-07-24 12:31:32 -0400

    Allow custom classes

--- render.js
+++ render.js
@@ -14,14 +14,14 @@ function render (tokens) {
     const { type } = line
 
     switch (type) {
-      case 'quote': return htmlEscape`<blockquote>${line.content}</blockquote>`
-      case 'header': return htmlEscape`<h${line.level} id="${id(line.content)}">${line.content}</h${line.level}>`
-      case 'link': return htmlEscape`<p><a href="${line.href}">${line.content}</a></p>`
+      case 'quote': return htmlEscape`<blockquote class="${line.className}">${line.content}</blockquote>`
+      case 'header': return htmlEscape`<h${line.level} id="${id(line.content)}" class="${line.className}">${line.content}</h${line.level}>`
+      case 'link': return htmlEscape`<p><a href="${line.href}" class="${line.className}">${line.content}</a></p>`
       case 'pre': return line.alt
-        ? htmlEscape`<pre><code class="language-${line.alt}">\n${line.items.join('\n')}\n</code></pre>`
-        : htmlEscape`<pre>\n${line.items.join('\n')}\n</pre>`
-      case 'list': return `<ul>\n${line.items.map((item) => htmlEscape`\t<li>${item}</li>`).join('\n')}\n</ul>`
-      default: return line.content ? htmlEscape`<p>${line.content}</p>` : '<br/>'
+        ? htmlEscape`<pre class="${line.className}"><code class="language-${line.alt}">\n${line.items.join('\n')}\n</code></pre>`
+        : htmlEscape`<pre class="${line.className}">\n${line.items.join('\n')}\n</pre>`
+      case 'list': return `<ul class="${line.className}">\n${line.items.map((item) => htmlEscape`\t<li>${item}</li>`).join('\n')}\n</ul>`
+      default: return line.content ? htmlEscape`<p class="${line.className}">${line.content}</p>` : '<br/>'
     }
   }).join('\n')
 }
